eeb2037dcfee5d08042bf3a08b9a2d608ba5a853
GUNNER9285 <GUNNER9285@gmail.com>
2019-06-07 22:56:53 +0700

    add Navbar

---
+++ src/main/java/com/training/werewolf/config/ThymeLeafConfig.java
@@ -0,0 +1,14 @@
+package com.training.werewolf.config;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ThymeLeafConfig {
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+}
+
---
+++ src/main/java/com/training/werewolf/controllers/GameController.java
@@ -0,0 +1,22 @@
+package com.training.werewolf.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping("/")
+public class GameController {
+    @GetMapping(value = "")
+    public String index() throws Exception {
+        return "sample/index";
+    }
+    @GetMapping(value = "cards")
+    public String showCards() throws Exception {
+        return "sample/card";
+    }
+}
+
